463d65d98c18c0afd166f3088b131e210941dbab
mikezalik <mike.zalik@gmail.com>
2020-03-04 09:53:54 -0800

    Adds string example to shared preference

--- app/src/main/java/com/mikezalik/sharedpreferencesdemo/MainActivity.java
+++ app/src/main/java/com/mikezalik/sharedpreferencesdemo/MainActivity.java
@@ -2,6 +2,8 @@ package com.mikezalik.sharedpreferencesdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SharedPreferences sharedPreferences = this.getSharedPreferences("com.mikezalik.sharedpreferencesdemo", Context.MODE_PRIVATE);
+
+        sharedPreferences.edit().putString("username", "Mike").apply();
+
+        sharedPreferences.getString("username", "");
     }
 }
